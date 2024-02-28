c2a9c9517aae8b87272df2319d40dfe713d6e091
Joe Copplestone <strasseneck@gmail.com>
2024-02-28 15:38:18 +0100

    Feat: Basic server running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,12 @@
-const express = require('express');
+import express, { Express, Request, Response } from "express";
 
+const app: Express = express();
+const PORT: number = 3000;
 
-const app = express();
+app.get("/", (req: Request, res: Response) => {
+    res.send('Hello Express with Typescript')
+});
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`)
+});
