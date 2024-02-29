c82e393b7f40c8f35d562da793d7eb1d1dcb52bd
Joe Copplestone <strasseneck@gmail.com>
2024-02-29 17:32:11 +0100

    Feat: Add addChange controller

--- server/src/controllers/changeController.ts
+++ server/src/controllers/changeController.ts
@@ -1,17 +1,23 @@
 import { Request, Response, NextFunction } from 'express'
+import Change from '../db/models/Change'
 
-export function getHello(req: Request, res: Response, next: NextFunction) {
-    res.json('Hello Change!')
-  }
-
-export function getChange (req: Request, res: Response, next: NextFunction ) {
+export async function getChange (req: Request, res: Response, next: NextFunction ) {
   return
 }
 
-export function addChange(req: Request, res: Response, next: NextFunction ) {
-  return
+export async function addChange(req: Request, res: Response, next: NextFunction ) {
+  try {
+    // create and save new change
+    const newChange = await Change.create(req.body);
+    // return the change
+    res.status(201);
+    res.json(newChange)
+  } catch (error) {
+    res.status(400);
+    res.json('Error saving change')
+  }
 }
   
-export function deleteChange (req: Request, res: Response, next: NextFunction ) {
+export async function deleteChange (req: Request, res: Response, next: NextFunction ) {
   return
 }
