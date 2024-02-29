e6699f95a7dd4ef4ca1557d3fef6bedfd311bfa9
Joe Copplestone <strasseneck@gmail.com>
2024-02-29 17:23:57 +0100

    Feat: Type change model

--- server/src/db/models/Change.ts
+++ server/src/db/models/Change.ts
@@ -1,19 +1,60 @@
 import { sequelize } from "../index"
-import { DataTypes } from "sequelize"
+import { DataTypes, Model } from "sequelize"
 
-const Change = sequelize.define('Change', {
+interface ChangeAttributes {
+    id: number,
+    description: string,
+    diffoperation: string,
+    difftext: string,
+    createdAt: Date,
+    updatedAt: Date,
+    DraftId: number,
+}
+
+class Change extends Model<ChangeAttributes> implements ChangeAttributes {
+    public id!: number; // using `!` to avoid TypeScript's strict null checking errors
+    public description!: string;
+    public diffoperation!: string;
+    public difftext!: string;
+    public createdAt!: Date; // Sequelize automatically adds it
+    public updatedAt!: Date; // Sequelize automatically adds it
+    public DraftId!: number;
+}
+
+Change.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
     description: {
         type: DataTypes.TEXT,
         allowNull: false,
     },
     diffoperation: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
         allowNull: false
     },
     difftext: {
         type: DataTypes.TEXT,
         allowNull: false
-    }
-});
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    DraftId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+ }, {
+        sequelize,
+        modelName: 'Change',
+    });
 
 export default Change 
--- server/src/db/models/Draft.ts
+++ server/src/db/models/Draft.ts
@@ -1,8 +1,6 @@
 import { sequelize } from "../index"
 import { DataTypes, Model } from "sequelize"
 import Change  from "./Change";
-// import { DraftType } from "../../controllers/draftController";
-
 
 interface DraftAttributes {
     id: number,
@@ -12,6 +10,7 @@ interface DraftAttributes {
     updatedAt: Date,
     UserId: number,
   }
+
   class Draft extends Model<DraftAttributes> implements DraftAttributes {
     public id!: number; // using `!` to avoid TypeScript's strict null checking errors
     public title!: string;
@@ -57,61 +56,3 @@ Draft.hasMany(Change);
 Change.belongsTo(Draft);
 
 export default Draft
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Draft = sequelize.define<Model<DraftType, DraftType>>('Draft', {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         allowNull: false,
-//         autoIncrement: true,
-//     },
-//     title: {
-//         type: DataTypes.STRING,
-//         allowNull: false, 
-//     },
-//     content: {
-//         type: DataTypes.TEXT, 
-//         allowNull: true,
-//     },
-//     createdAt: {
-//         type: DataTypes.DATE,
-//         allowNull: false,
-//     },
-//     updatedAt: {
-//         type: DataTypes.DATE,
-//         allowNull: false
-//     },
-//     UserId: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         references: { model: 'User', key: 'id'}
-//     }
-// });
-
-// Draft.hasMany(Change);
-// Change.belongsTo(Draft);
-
-
-
-// export default Draft 
