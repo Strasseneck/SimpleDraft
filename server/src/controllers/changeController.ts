bdcf2aee4e383cfec09eea838e3113854e845ffd
Joe Copplestone <strasseneck@gmail.com>
2024-02-29 17:39:33 +0100

    Feat: Add getChange controller

--- server/src/controllers/changeController.ts
+++ server/src/controllers/changeController.ts
@@ -2,7 +2,24 @@ import { Request, Response, NextFunction } from 'express'
 import Change from '../db/models/Change'
 
 export async function getChange (req: Request, res: Response, next: NextFunction ) {
-  return
+  const changeId = req.params.id;
+  try {
+    // try to retrieve
+    const change = await Change.findOne({where: {id: changeId }})
+    if (!change) {
+      // doesn't exist, return error msg
+      res.status(400);
+      res.json('No change with that id!');
+    }
+    else {
+      // exists return change
+      res.status(200)
+      res.json(change);
+    }
+  } catch (error) {
+    res.status(400);
+    res.json(`Error retrieving change ${error}`)
+  }
 }
 
 export async function addChange(req: Request, res: Response, next: NextFunction ) {
@@ -14,7 +31,7 @@ export async function addChange(req: Request, res: Response, next: NextFunction 
     res.json(newChange)
   } catch (error) {
     res.status(400);
-    res.json('Error saving change')
+    res.json(`Error saving change ${error}`)
   }
 }
   
