bc43afb85a8a6861a23de069f8a7edd58258237d
Joe Copplestone <strasseneck@gmail.com>
2024-03-01 13:49:23 +0100

    Feat: add todos to dashboard view

--- client/src/views/DashboardView.tsx
+++ client/src/views/DashboardView.tsx
@@ -1,16 +1,26 @@
 import React, { FC } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import EditorView from './EditorView';
+// import { Draft } from '../apiService/responseTypes';
 
 const DashboardView: FC = () => {
   const [draftId, setDraftId] = useState<number | null>(null);
+  // const [usersDrafts, setUsersDrafts] = useState<Draft[]>([])
+
+  // const handleOnClick = () => {
+  //   // TODO
+  //   // open modal that creates new draft
+  //   // create draft
+  //   // redirect to editor with new draft id
+  // }
   
   const openEditor = (id: number) => {
     setDraftId(id)
   }
   return (
     <div>
-      <p>Dashboard Working</p>
+      {/* <button onClick={handleOnClick}>New Draft</button> */}
+      <p>TODO A grid showing all the current users drafts</p>
       <button onClick={() => openEditor(1)}>Open Editor</button>
       {draftId !== null && <EditorView id={draftId} />}
     </div>
