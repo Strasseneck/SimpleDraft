c1dac86e4e32606777e9b72ca24fa7fd87730e63
Joe Copplestone <strasseneck@gmail.com>
2024-03-01 11:10:21 +0100

    Feat: Add basic state

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react';
 
+// types the props, representing the draft to be edited
 interface EditorProps {
   workingDraft: string,
   onWorkingDraftChange: (content: string) => void;
 }
 
 const Editor: FC<EditorProps> = ({ workingDraft, onWorkingDraftChange }) => {
+  // handle changes made in the text area
   const handleChange = ( event: React.ChangeEvent<HTMLTextAreaElement> ) => {
     const content = event.target.value;
     onWorkingDraftChange(content);
   }
 
   return (
-    <textarea value={workingDraft} onChange={handleChange} />
+    <textarea className='main-editor' value={workingDraft} onChange={handleChange} />
   );
 };
 
--- client/src/views/EditorView.tsx
+++ client/src/views/EditorView.tsx
@@ -1,12 +1,33 @@
 // EditorView.tsx
 import React, { useState, useEffect } from 'react';
 import Editor from '../components/Editor';
+import { getDraft } from '../apiService/DraftApi';
 
-const EditorView: React.FC = () => {
-  const [draft, setDraft] = useState<string>('Initial draft');
+interface EditorViewProps {
+  draftid: number,
+}
+
+const EditorView: React.FC<EditorViewProps> = ({ draftid }) => {
+  const [draft, setDraft] = useState<string>('My Great Project');
   const [workingDraft, setWorkingDraft] = useState<string>(draft);
 
   // use effect to get draft
+  useEffect(() => {
+    async function retrieveDraft () {
+      try {
+        // get draft from api
+        const draft = await getDraft(draftid);
+        // set both states to the content returned
+        setDraft(draft.content)
+      } catch (error) {
+        console.error(`Error retrieving draft with id: ${draftid} ${error}`)
+      }
+    };
+
+    if (draftid) {
+      retrieveDraft()
+    }
+  }, [draftid]);
 
   // update working draft
   const handleWorkingDraftChange = (content: string) => {
