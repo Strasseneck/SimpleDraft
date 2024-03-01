41265b308004b385c15190107c16594b25af9dd2
Joe Copplestone <strasseneck@gmail.com>
2024-03-01 09:59:45 +0100

    Feat: Add basic EditorView logic, boilerplate code

--- client/src/views/EditorView.tsx
+++ client/src/views/EditorView.tsx
@@ -1,10 +1,22 @@
-import React, { FC } from 'react';
+// EditorView.tsx
+import React, { useState, useEffect } from 'react';
+import Editor from '../components/Editor';
 
-const EditorView: FC = () => {
+const EditorView: React.FC = () => {
+  const [draft, setDraft] = useState<string>('Initial draft');
+  const [workingDraft, setWorkingDraft] = useState<string>(draft);
+
+  // use effect to get draft
+
+  // update working draft
+  const handleWorkingDraftChange = (content: string) => {
+    setWorkingDraft(content);
+  };
+
   return (
     <div>
-      <p>Editor Working</p>
-      {/* JSX content for the view here */}
+      <h1>Editor View</h1>
+      <Editor workingDraft={workingDraft} onWorkingDraftChange={handleWorkingDraftChange} />
     </div>
   );
 };
