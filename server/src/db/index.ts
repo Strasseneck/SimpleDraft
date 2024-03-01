6247fbae0c0e3b28125d8ebfd5a77f292aca1e8a
Joe Copplestone <strasseneck@gmail.com>
2024-03-01 10:34:45 +0100

    Feat: Change db to use .env

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-import { password } from './models/password';
+import { config } from "dotenv"; 
+config();
 
-const sequelize = new Sequelize('drafts', 'strasseneck', password, {
+const sequelize = new Sequelize( process.env.DB!, process.env.USER!, process.env.PASSWORD!, {
     host: '127.0.0.1',
     dialect: 'postgres',
     logging: (...msg) => console.log(msg)
