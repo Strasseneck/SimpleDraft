0fd7c3226bd181106cb65f397c20063b6d0c30a8
Joe Copplestone <strasseneck@gmail.com>
2024-03-01 12:57:57 +0100

    Fix: Move rendering of EditorView from the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardView from './views/DashboardView';
-import EditorView from './views/EditorView';
 import ChangeHistoryView from './views/ChangeHistoryView';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/editor" element={<EditorView />} />
         <Route path="/change-history" element={<ChangeHistoryView />} />
         <Route path="/" element={<DashboardView />} />
       </Routes>
