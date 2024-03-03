a33f95ff16e5863c308d495b9afd36f961b5c726
Joe Copplestone <strasseneck@gmail.com>
2024-03-03 14:10:40 +0100

    Fix: Fix the resizing and scroll behaviour of the SingleChangePage

--- client/src/components/SingleChangePage.tsx
+++ client/src/components/SingleChangePage.tsx
@@ -1,22 +1,24 @@
-import { FC } from 'react';
-import './SingleChangePage.css'
+import { FC, useEffect } from 'react';
+import './SingleChangePage.css';
 
 interface Props {
     sanitizedHtmlDiffs: string;
 }
 
 const SingleChangePage: FC<Props> = ({ sanitizedHtmlDiffs }) => {
-    
-    const calculateHeight = () => {
-        const rows = sanitizedHtmlDiffs.split('\n').length;
-        const lineHeight = 20;
-        const minHeight = 100;
-        const height = Math.max(minHeight, rows * lineHeight);
-        return `${height}px`;
-    };
+    useEffect(() => {
+        const contentElement = document.querySelector('.SingleChangePage') as HTMLElement;
+        if (contentElement) {
+            contentElement.style.height = "auto";
+            contentElement.style.height = `${contentElement.scrollHeight}px`;
+        }
+    }, []);
 
     return (
-        <div className='SingleChangePage' dangerouslySetInnerHTML={{ __html: sanitizedHtmlDiffs }} style={{ height: calculateHeight() }} />
+        <div
+            className='SingleChangePage'
+            dangerouslySetInnerHTML={{ __html: sanitizedHtmlDiffs }}
+        />
     );
 };
 
