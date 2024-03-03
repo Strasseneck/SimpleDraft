c37c2be2b51011ff1756ed69d8e1940a8648808e
Joe Copplestone <strasseneck@gmail.com>
2024-03-03 10:43:11 +0100

    Feat: Dashboard basically done, styled with navigation

--- client/src/components/DraftCard.tsx
+++ client/src/components/DraftCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import moment from 'moment';
 import { DraftResponse } from '../apiService/responseTypes';
+import { useNavigate } from 'react-router-dom';
 import './DraftCard.css'
 
 interface Props {
@@ -8,15 +9,24 @@ interface Props {
 }
 
 const DraftCard: FC<Props> = ({ draft }) => {
-  const { createdAt, updatedAt, Changes } = draft;
+  const { id, createdAt, updatedAt, Changes } = draft;
+  const navigate = useNavigate();
+
+
   const formattedCreatedAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
   const formattedUpdatedAt = moment(updatedAt).format('MMMM Do YYYY, h:mm:ss a');
   const changeCount = Changes.length;
   const lastChange = Changes.length > 0 ? Changes[0].description : 'No changes yet';
 
+  const handleTitleClick = () => { 
+    navigate('/editor', { state: { id} });
+  }
+
   return (
     <div className='DraftCard'>
-      <h3 className='heading'>Title: { draft.title }</h3>
+      <a onClick={handleTitleClick} style={{ cursor: 'pointer' }}>
+        <h3 className='heading'>Title: { draft.title }</h3>
+      </a>
       <p className='heading'>Created:</p>
       <p>{ formattedCreatedAt}</p>
       <p className='heading'>Total Changes:</p>
