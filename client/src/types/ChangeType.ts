b5b990b92e7d6b16cfec440d75504f4c22f66f18
Joe Copplestone <strasseneck@gmail.com>
2024-03-05 09:13:31 +0100

    Feat: Add patchObject to Change type

--- client/src/types/ChangeType.ts
+++ client/src/types/ChangeType.ts
@@ -1,9 +1,10 @@
-import { Diff } from "diff-match-patch-typescript";
+import { Diff, PatchObject } from "diff-match-patch-typescript";
 
 interface ChangeType {
     description: string,
     DraftId: number,
     Diffs: Diff[]
+    Patches: PatchObject[];
 }
 
 export default ChangeType;
